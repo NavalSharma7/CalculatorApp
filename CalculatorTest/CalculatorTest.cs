380cd195d756a1edb9d30eb3480878efd286559c
NavalSharma7 <A00241484@myCambrian.ca>
2021-10-31 23:03:42 -0400

    unit tests for subtraction
    Added unit tests for subtraction - one data test method and one simple test method

--- CalculatorTest/CalculatorTest.cs
+++ CalculatorTest/CalculatorTest.cs
@@ -46,6 +46,28 @@ namespace CalculatorTest
 
         //unit test methods for subtraction.
 
+        [TestMethod]
+        public void SubtractMethodMustSubtract()
+        {
+            var result = Calculator.Subtract(2, 1);
+            Assert.AreEqual(1, result);
+
+        }
+
+
+        [DataTestMethod]
+        [DataRow(-1, -2, 1)]
+        [DataRow(-11, -24, 13)]
+        [DataRow(-120, -180, 60)]
+        [DataRow(-1.33, -2.33, 1)]
+
+        public void SubtractTwoNegativeNumbers(double num1, double num2, double expected)
+        {
+            var result = Calculator.Subtract(num1, num2);
+            Assert.AreEqual(expected, result);
+
+        }
+
         //unit test methods for multiplication.
 
         //unit test methods for division.
