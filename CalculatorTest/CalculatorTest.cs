3897c7062718afd3acd438fb55be2f87502ffa21
NavalSharma7 <A00241484@myCambrian.ca>
2021-10-31 23:12:29 -0400

    added multiplication unit tests
    Added unit tests for multiplication including one test method and one data test method

--- CalculatorTest/CalculatorTest.cs
+++ CalculatorTest/CalculatorTest.cs
@@ -70,6 +70,27 @@ namespace CalculatorTest
 
         //unit test methods for multiplication.
 
+        [TestMethod]
+        public void MultiplyMethodMustMultiply()
+        {
+            var result = Calculator.Multiply(1, 5);
+            Assert.AreEqual(5, result);
+
+        }
+
+
+        [DataTestMethod]
+        [DataRow(-1, -2, 2)]
+        [DataRow(-11, -4, 44)]
+        [DataRow(-100, -180, 18000)]
+        [DataRow(-1.1, -2.0, 2.2)]
+
+        public void MultiplyTwoNegativeNumbers(double num1, double num2, double expected)
+        {
+            var result = Calculator.Multiply(num1, num2);
+            Assert.AreEqual(expected, result);
+
+        }
         //unit test methods for division.
 
     }
